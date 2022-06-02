020e7b13ccd1414259ca63b6f56a97f2585bafad
wilfredomateo <wmateo86@gmail.com>
2022-06-02 06:56:56 -0500

    added message mixer program and all relevant files

---
+++ MessageMixer/message-mixer.js
@@ -0,0 +1,6 @@
+//Message Mixer Program
+/////////////////////////
+//Use: Message encryption that transforms input text, using various ciphers, and displays the encrypted message to the console.
+//Author: Wilfredo Mateo
+
+
