bd9dcba301999d8aaf48f158417b058e72b8985d
wilfredomateo <wmateo86@gmail.com>
2022-07-09 11:08:30 -0500

    finished the animal fact webpage

--- AnimalFacts/files/app.js
+++ AnimalFacts/files/app.js
@@ -1,1 +1,41 @@
 import { animals } from './animals';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = '';
+const background = <img className='background' alt='ocean' src='/images/ocean.jpg'/>
+
+const images = [];
+for (const animal in animals) {
+  images.push(
+    <img 
+      key={animal}
+      className='animal' 
+      alt={animal}
+      src={animals[animal].image} 
+      aria-label={animal} 
+      role='button'
+      onClick = {displayFact}
+    />
+  );
+}
+
+const animalFacts = ( 
+  <div>
+    {background},
+    <div className='animals'>{images}</div>
+     <p id='fact'></p>
+    <h1>{title === '' ? 'Click an animal for a fun fact' : title}</h1>
+  </div>
+  );
+
+function displayFact(e) {
+  const selectedAnimal = e.target.alt;
+  const animalInfo = animals[selectedAnimal];
+  const optionIndex = Math.floor(Math.random() * animalInfo.facts.length);
+  const funFact = animalInfo.facts[optionIndex];
+  document.getElementById('fact').innerHTML = funFact;
+}
+
+ReactDOM.render(animalFacts, document.getElementById('root'));
+
